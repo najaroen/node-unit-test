76b5009fb818e2edd8f208d3ca4801aa20de5847
TIKJA <najaroen_tik@live.com>
2020-11-30 11:48:39 +0700

    Before&After

--- test/test.js
+++ test/test.js
@@ -1,6 +1,18 @@
 const assert = require('assert')
 describe('Hell', ()=> {
     context('function to be tested', () => {
+        before(()=> {
+            console.log('before===>')
+        })
+        after(()=> {
+            console.log('after===>')
+        })
+        beforeEach(() => {
+            console.log('====>beforeEach')
+        })
+        afterEach(() => {
+            console.log('=====>afterEach')
+        })
         it('should do someting', ()=>{
             assert.equal(1,1)
         });
